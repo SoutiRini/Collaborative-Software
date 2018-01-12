5c4f2d4c757670f49be43cb7ac0fa17cc108e349
Caius Brindescu <brindesc@oregonstate.edu>
2018-01-12 15:18:20 -0800

    Reading the projects from the file

--- apache-ml-scraper/scraper.py
+++ apache-ml-scraper/scraper.py
@@ -1,17 +1,34 @@
 from pyquery import PyQuery as pq
 import urllib.request
+import os
 
 root = 'http://mail-archives.apache.org/mod_mbox'
-project = 'cordova'
 
-d = pq(root + '/' + project + '-dev')
+dataPath = "/Volumes/GoogleDrive/Team Drives/Reflection Distributed Development/data/"
+dataPath = os.path.join(dataPath, 'dev-ml')
 
-months = [x.attr('href').split('/')[0] for x in d('.year td a').items() if x.text() == 'Date']
+if not os.path.exists(dataPath):
+    os.mkdir(dataPath)
 
-for month in months:
-    response = urllib.request.urlopen(root + '/' + project + '/' + month)
-    with open('data/' + month, 'wb') as f:
-        f.write(response.read())
+with open('../apache-projects.txt', 'r', encoding='utf-16') as f:
+    for project in f:
+        project = project.strip();
+        projectPath = os.path.join(dataPath, project)
+        if not os.path.exists(projectPath):
+            os.mkdir(projectPath)
+
+        projUrl = root + '/' + project + '-dev'
+        print("GET: " + projUrl)
+        d = pq(projUrl)
+
+        months = [x.attr('href').split('/')[0] for x in d('.year td a').items() if x.text() == 'Date']
+
+        for month in months:
+            monthUrl = root + '/' + project + '-dev/' + month
+            print("GET: " + monthUrl)
+            response = urllib.request.urlopen(monthUrl)
+            with open(os.path.join(projectPath, month), 'wb') as f:
+                f.write(response.read())
 
 
 #http://mail-archives.apache.org/mod_mbox/cordova-dev/201801.mbox/raw/%3c166866877.869.1514775098330.JavaMail.jenkins@jenkins-master.apache.org%3e
