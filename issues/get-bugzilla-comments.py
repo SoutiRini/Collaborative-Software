62a778a9615fa83901e48482afb883e318efa4b9
Caius Brindescu <brindesc@oregonstate.edu>
2018-01-22 10:56:32 -0800

    Using tor for this one too

--- issues/get-bugzilla-comments.py
+++ issues/get-bugzilla-comments.py
@@ -1,5 +1,5 @@
 import json
-import requests as req
+from torrequest import TorRequest
 from urllib.parse import urlunparse
 from random import randint
 from time import sleep
@@ -10,13 +10,23 @@ with open('bugzilla_bugs.json', 'r') as f:
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7'}
 comments = {}
 
-for bug in bugs[0]:
-    urlbits = ['https', 'issues.apache.org', '/bugzilla/rest.cgi/bug/' + str(bug) + '/comment', '', '', '']
-    url = urlunparse(urlbits)
-    print(url)
-    resp = req.get(url, headers=headers)
-    comments[str(bug)] = resp.json()['bugs'][str(bug)]['comments']
-    sleep(randint(1,20))
+proxyPort = randint(9000, 45000)
+controlPort = proxyPort + randint(1,10000)
+
+with TorRequest(proxyPort=proxyPort, controlPort=controlPort, password=None) as tr:
+    for bug in bugs[0]:
+        urlbits = ['https', 'issues.apache.org', '/bugzilla/rest.cgi/bug/' + str(bug) + '/comment', '', '', '']
+        url = urlunparse(urlbits)
+        print(url)
+        try:
+            resp = tr.get(url, headers=headers)
+        except:
+            print("Something went wrong. Trying again")
+            sleep(randint(1,10))
+            tr.reset_identity()
+        comments[str(bug)] = resp.json()['bugs'][str(bug)]['comments']
+        sleep(randint(1,10))
+        tr.reset_identity()
 
 with open('bugzilla_comments.json', 'w') as f:
     json.dump(comments, f)
