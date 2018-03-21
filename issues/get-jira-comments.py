dbc9872d392e1fb9b076162dffad7ab8f6c44543
Caius Brindescu <brindesc@oregonstate.edu>
2018-03-20 17:09:51 -0700

    More error checking to improve robustness

--- issues/get-jira-comments.py
+++ issues/get-jira-comments.py
@@ -16,18 +16,23 @@ comment_folder = 'jira_comments'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7'}
 
 def getForProject(path):
-    proxyPort = randint(10000, 20000)
-    controlPort = proxyPort + randint(1,10000)
     tid = randint(1, 55053)
     data_dir = '~/.tor/data' + str(tid) + '/'
-    print('Opening tor at proxy port ' + str(proxyPort) + ' and control port ' + str(controlPort))
     shutil.copytree(os.path.expanduser('~/.tor/template/'), os.path.expanduser(data_dir))
-    tor_proc = launch_tor_with_config(config=
-        {
-            'SocksPort': str(proxyPort),
-            'ControlPort': str(controlPort),
-            'DataDirectory': data_dir
-        }, take_ownership=True)
+    while True:
+        try:
+            proxyPort = randint(10000, 20000)
+            controlPort = proxyPort + randint(1,10000)
+            print('Opening tor at proxy port ' + str(proxyPort) + ' and control port ' + str(controlPort))
+            tor_proc = launch_tor_with_config(config=
+                {
+                    'SocksPort': str(proxyPort),
+                    'ControlPort': str(controlPort),
+                    'DataDirectory': data_dir
+                }, take_ownership=True)
+            break
+        except:
+            print("Failed to open tor. Trying again")
     print("Tor opened successfully")
     ctrl = Controller.from_port(port=controlPort)
     ctrl.authenticate(password=None)
@@ -54,7 +59,11 @@ def getForProject(path):
                     sleep(randint(1,10))
                     ctrl.signal(Signal.NEWNYM)
                     count = count + 1
-            j = resp.json()
+            try:
+                j = resp.json()
+            except:
+                j = {}
+
             fullIssues.append(j)
             sleep(randint(1,10))
             ctrl.signal(Signal.NEWNYM)
@@ -62,11 +71,11 @@ def getForProject(path):
     session.close()
     ctrl.close()
     tor_proc.terminate()
-    shutil.rmtree(data_dir)
+    shutil.rmtree(os.path.expanduser(data_dir))
 
     with open(os.path.join(comment_folder, os.path.basename(path)), 'w') as f:
         json.dump(fullIssues, f)
 
 if __name__ == '__main__':
-    p = Pool(16)
+    p = Pool(32)
     p.map(getForProject, [entry.path for entry in os.scandir(issueFolder) if entry.is_file()])
